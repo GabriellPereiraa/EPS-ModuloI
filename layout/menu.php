aa50dcd904db39fc77448f0b04c5d2f75a4756ac
Gabriel Pereira <78962152+GabriellPereiraa@users.noreply.github.com>
2021-02-16 14:11:07 -0300

    Start

---
+++ layout/menu.php
@@ -0,0 +1,28 @@
+<div id="app">
+    <div id="sidebar">
+        <div class="sidebar-wrapper active ps ps--active-y">
+            <div class="m-4">
+                <a href="index.php">
+                    <img class="img-fluid" src="https://atilasilvalima.a2hosted.com/projects/EGPCE/Cedis/cdn/imagens/egpce.png" alt="Logo EGPCE" srcset="">
+                </a>
+            </div>
+            <br>
+            <div class="sidebar-menu ">
+                <ul class="menu">
+                    <!--  <li class="sidebar-item">
+                        <a href="/Apresentacao.php" class="sidebar-link">
+                            <i data-feather="home" width="20"></i>
+                            <span>Apresentação</span>
+                        </a>
+                    </li> -->
+                    <li class="sidebar-item">
+                        <a href="AcessoInformacao.php" class="sidebar-link">
+                            <i data-feather="home" width="20"></i>
+                            <span>Módulo II - Acesso à Informação</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+        </div>
+    </div>
